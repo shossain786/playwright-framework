303cae7b44440abf1f8fd9c016ecf7bf34c01e59
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-02-28 07:56:47 +0530

    test data added

---
+++ src/data/testData.ts
@@ -0,0 +1,31 @@
+export const testData = {
+  validUser: {
+    username: 'admin',
+    password: 'password',
+    firstName: 'Rahul',
+    lastName: 'Singh',
+  },
+  invalidUser: {
+    username: 'invalid@cc.com',
+    password: 'wrongpassword',
+  },
+  emptyUser: {
+    username: '',
+    password: '',
+  },
+  registration: {
+    newUser: {
+      firstName: 'Komal',
+      lastName: 'Pal',
+      email: `komal.pal@n.com`,
+      password: 'Test@1234',
+    },
+  },
+  errorMessages: {
+    invalidCredentials: 'Invalid username or password',
+    emptyUsername: 'Username is required',
+    emptyPassword: 'Password is required',
+  },
+};
+
+export type TestData = typeof testData;
